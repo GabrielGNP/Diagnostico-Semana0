36ceca14302cfe9e208a92dfcf37fff46d50d51a
Javier Andrés Luis González <javierandresluisgonzalez@gmail.com>
2026-02-27 14:01:49 -0500

    feat: modify cors to frontend

--- Backend/usuario-service/src/main/java/com/example/usuarioservice/config/CorsConfig.java
+++ Backend/usuario-service/src/main/java/com/example/usuarioservice/config/CorsConfig.java
@@ -10,7 +10,7 @@ import org.springframework.web.filter.CorsFilter;
 @Configuration
 public class CorsConfig {
 
-    @Value("${app.cors.allowed-origins:http://localhost:3001,http://localhost:3000}")
+    @Value("${app.cors.allowed-origins:http://localhost:3001,http://localhost:3000,http://localhost:5173}")
     private String allowedOrigins;
 
     @Bean
